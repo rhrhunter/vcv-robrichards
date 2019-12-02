84e4b56c443929d3a0bf47fc57f4908ed940ec24
rhrhunter <rh.rhunter@gmail.com>
2019-12-01 19:08:22 -0500

    More tests

--- test/build-ubuntu1804-rack116.sh
+++ test/build-ubuntu1804-rack116.sh
@@ -2,5 +2,4 @@
 
 docker build \
        --build-arg TAG="ubuntu18.04-rack1.1.6" \
-       --no-cache \
        -t vcv-robrichards-build:latest .
--- test/build-ubuntu1904-rack116.sh
+++ test/build-ubuntu1904-rack116.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build \
-       --build-arg TAG="ubuntu19.10-rack1.1.6" \
+       --build-arg TAG="ubuntu19.04-rack1.1.6" \
        -t vcv-robrichards-build:latest .
