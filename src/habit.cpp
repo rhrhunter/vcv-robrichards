d9efa7cdbfded7066c0947cb98910ef35a6c0f71
rhrhunter <rh.rhunter@gmail.com>
2022-04-24 14:32:08 -0400

    defaults

--- src/habit.cpp
+++ src/habit.cpp
@@ -63,12 +63,12 @@ struct Habit : RRModule {
     config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
     // main knob parameters
-    configParam(LEVEL_PARAM, 0.f, 127.f, 0.f, "Volume Level (Wet/Dry)");
-    configParam(REPEATS_PARAM, 0.f, 127.f, 64.f, "Repeats (0 -> Infinite)");
-    configParam(SIZE_PARAM, 0.f, 127.f, 0.f, "Size of each repeat (50ms -> 60s)");
-    configParam(MODIFY_PARAM, 0.f, 127.f, 0.f, "Modify");
-    configParam(SPREAD_PARAM, 0.f, 127.f, 64.f, "Spread");
-    configParam(SCAN_PARAM, 0.f, 127.f, 64.f, "Scan");
+    configParam(LEVEL_PARAM, 0.f, 127.f, 64.f, "Volume Level (Wet/Dry)");
+    configParam(REPEATS_PARAM, 0.f, 127.f, 0.f, "Repeats (0 -> Infinite)");
+    configParam(SIZE_PARAM, 0.f, 127.f, 32.f, "Size of each repeat (50ms -> 60s)");
+    configParam(MODIFY_PARAM, 0.f, 127.f, 64.f, "Modify");
+    configParam(SPREAD_PARAM, 0.f, 127.f, 0.f, "Spread");
+    configParam(SCAN_PARAM, 0.f, 127.f, 0.f, "Scan");
 
     // 3 way switches
     // 1.0f is top position
